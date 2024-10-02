9d4af41eb0d7c274bd821ee0c47368d7d8fff942
lee-dong-gi <oksk327@naver.com>
2024-10-02 23:31:07 +0900

    feat: added bill folding (by programmers) - [PCCE 기출문제] 9번 / 지폐 접기

---
+++ src/main/java/com/study/testcode/programmers/BillFolding.java
@@ -0,0 +1,37 @@
+package com.study.testcode.programmers;
+
+import org.springframework.stereotype.Service;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/340199
+@Service
+public class BillFolding {
+
+    public int solution(int[] wallet, int[] bill) {
+        int answer = 0;
+        int[] foldBill = {bill[0], bill[1]};
+
+        while (checkScale(wallet, foldBill)) {
+            if (foldBill[0] > foldBill[1]) {
+                foldBill[0] = foldBill[0] / 2;
+            } else {
+                foldBill[1] = foldBill[1] / 2;
+            }
+            answer++;
+        }
+
+        return answer;
+    }
+
+    private static boolean checkScale(int[] wallet, int[] foldBill) {
+        return getSmallestValue(foldBill) > getSmallestValue(wallet)
+                || getLargestValue(foldBill) > getLargestValue(wallet);
+    }
+
+    private static int getSmallestValue(int[] target) {
+        return Math.min(target[0], target[1]);
+    }
+
+    private static int getLargestValue(int[] target) {
+        return Math.max(target[0], target[1]);
+    }
+}
---
+++ src/test/java/com/study/testcode/programmers/BillFoldingTest.java
@@ -0,0 +1,27 @@
+package com.study.testcode.programmers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static com.study.testcode.common.constant.DateConstants.CONFIG_TEST;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@TestPropertySource(locations = CONFIG_TEST)
+public class BillFoldingTest {
+    @Autowired
+    private BillFolding billFolding;
+
+    @Test
+    void targetNumberTest() {
+        int[] wallet1 = {30, 15};
+        int[] bill1 = {26, 17};
+        assertEquals(1, billFolding.solution(wallet1, bill1));
+
+        int[] wallet2 = {50, 50};
+        int[] bill2 = {100, 241};
+        assertEquals(4, billFolding.solution(wallet2, bill2));
+    }
+}
